4e1d9a13ad55bd8fd532100e79e2296fffe14032
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2018-08-23 15:05:36 +0700

    Add files via upload

--- teensy/firmware/lib/config/lino_base_config.h
+++ teensy/firmware/lib/config/lino_base_config.h
@@ -8,8 +8,8 @@
 // #define LINO_BASE MECANUM         // Mecanum drive robot
 
 //uncomment the motor driver you're using
-#define USE_L298_DRIVER
-// #define USE_BTS7960_DRIVER
+// #define USE_L298_DRIVER
+#define USE_BTS7960_DRIVER
 // #define USE_ESC
 
 //uncomment the IMU you're using
@@ -18,19 +18,19 @@
 // #define USE_MPU9150_IMU
 // #define USE_MPU9250_IMU
 
-#define DEBUG 1
+#define DEBUG 0
 
 #define K_P 0.6 // P constant
 #define K_I 0.3 // I constant
 #define K_D 0.5 // D constant
 
 //define your robot' specs here
-#define MAX_RPM 330               // motor's maximum RPM
-#define COUNTS_PER_REV 1550       // wheel encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.10       // wheel's diameter in meters
+#define MAX_RPM 181               // motor's maximum RPM
+#define COUNTS_PER_REV 2000       // wheel encoder's no of ticks per rev
+#define WHEEL_DIAMETER 0.13648176796       // wheel's diameter in meters
 #define PWM_BITS 8                // PWM Resolution of the microcontroller
-#define LR_WHEELS_DISTANCE 0.235  // distance between left and right wheels
-#define FR_WHEELS_DISTANCE 0.30   // distance between front and rear wheels. Ignore this if you're on 2WD/ACKERMANN
+#define LR_WHEELS_DISTANCE 0.38966  // distance between left and right wheels
+#define FR_WHEELS_DISTANCE 0.375   // distance between front and rear wheels. Ignore this if you're on 2WD/ACKERMANN
 
 //=================BIGGER ROBOT SPEC (BTS7960)=============================
 // #define K_P 0.05  // P constant
@@ -107,8 +107,8 @@ ROBOT ORIENTATION
   #define MOTOR3_IN_B 23
 
   #define MOTOR4_PWM 2 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR4_IN_A 4
-  #define MOTOR4_IN_B 3
+  #define MOTOR4_IN_A 25 //4
+  #define MOTOR4_IN_B 32 //3
 
   #define PWM_MAX pow(2, PWM_BITS) - 1
   #define PWM_MIN -PWM_MAX
